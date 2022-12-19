c0bf89e607a55606ec806cc6b0e5ba7dcc40776c
Jane49-cloud <janendirangu49@gmail.com>
2022-12-19 16:31:05 +0300

    color theme and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,21 @@ import {BrowserRouter,Routes, Route} from "react-router-dom"
 import HomePage  from "./components/HomePage"
 import LoginPage from "./components/LoginPage"
 import ProfilePage  from "./components/ProfilePage"
+import {useMemo } from "react"
+import {CssBaseline, ThemeProvider} from "@mui/material"
+import {createTheme} from "@mui/material/styles"
+import {themeSettings} from "./theme.js"
+import {useSelector} from "react-redux"
 
 function App() {
+const mode =useSelector((state)=>state.mode)
+const theme = useMemo(()=>createTheme(themeSettings(mode)), [mode])
+
   return (
     <div className="App">
     <BrowserRouter>
+    <ThemeProvider theme={theme}>
+    <CssBaseline/>
       <Routes>
         <Route path="/" element = {<LoginPage/>}/>
       </Routes>
@@ -16,6 +26,7 @@ function App() {
       <Routes>
         <Route path="/profile/:userid" element = {<ProfilePage/>}/>
       </Routes>
+      </ThemeProvider>
     </BrowserRouter>
     </div>
   );
